f8820da8d65e0e734d58dcbbb57a872ca6e73f4a
PewDizinho <konopkspaulo046@gmail.com>
2024-05-07 16:33:02 -0300

    Screen not resizable

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 
+
+
 function createWindow() {
+
+
   const win = new BrowserWindow({
-      fullscreen: true,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
+    height: 1400,
+    width: 900,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+
+  })
 
   win.loadFile('./lib/index.html')
 }
@@ -17,13 +24,16 @@ app.whenReady().then(() => {
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
+
     }
   })
 })
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
