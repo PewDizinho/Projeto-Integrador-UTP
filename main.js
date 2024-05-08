224722bf5f7915e49ded0e96373227d7332ba93f
PewDizinho <konopkspaulo046@gmail.com>
2024-05-07 22:54:42 -0300

    Question v1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 
+// const { screen } = require('electron');
 
 
 function createWindow() {
@@ -10,15 +11,33 @@ function createWindow() {
     height: 1400,
     width: 900,
     resizable: false,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
 
   })
 
-  win.loadFile('./lib/index.html')
-}
+  win.loadFile('./lib/room1/index.html');
 
+  // win.on('close', (e) => {
+  //   win.setTitle('I do not want to be closed');
+  //   let mainScreen = screen.getPrimaryDisplay();
+  //   let dimensions = mainScreen.size;
+
+  //   const safeArea = {
+  //     x: [0.1, 0.3],
+  //     y: []
+  //   };
+  //   let desiredX = Math.round(dimensions.width * Math.random(0.8)); 
+  //   let desiredY = Math.round(dimensions.height * Math.random(0.5));
+
+  //   win.setPosition(desiredX, desiredY);
+
+  //   e.preventDefault();
+
+  // })
+}
 app.whenReady().then(() => {
   createWindow()
 
