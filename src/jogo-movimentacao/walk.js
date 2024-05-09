9db689db6f06f52b74f1d6996b5f0feaa42d8607
PewDizinho <konopkspaulo046@gmail.com>
2024-05-09 00:42:18 -0300

    resized the screen to 900x700

--- src/jogo-movimentacao/walk.js
+++ src/jogo-movimentacao/walk.js
@@ -1,4 +1,4 @@
-let playerPosition = [60, 400]; //X, Y
+let playerPosition = [60, 380]; //X, Y
 const playerObj = document.getElementById("player");
 const playerSpeed = 20;
 let jumpInterval, fallInterval, enemyInterval, enemyPosition;
@@ -11,18 +11,13 @@ document.addEventListener("keydown", (e) => {
             if (isOnAir || gameSituation != "playing") return;
             jump();
         },
-      
-        
-
     };
     if (e.keyCode == 32) {
         clearInterval(enemyInterval);//debug
     }
     keysAction[e.key] && keysAction[e.key]();
 
-
 })
-
 
 const jump = (e) => {
     let jumpHeight = 100;
@@ -46,10 +41,10 @@ const fall = () => {
     fallInterval = setInterval(() => {
         playerPosition[1] += fallSpeed;
         playerObj.style.margin = `${playerPosition[1]}px ${playerPosition[0]}px`;
-        if (playerPosition[1] >= 400) {
+        if (playerPosition[1] >= 380) {
             clearInterval(fallInterval);
-            playerPosition[1] = 400;
-            playerObj.style.margin = `400px ${playerPosition[0]}px`;
+            playerPosition[1] = 380;
+            playerObj.style.margin = `380px ${playerPosition[0]}px`;
             isOnAir = false;
         }
     }, 1);
@@ -65,7 +60,6 @@ const enemyLifeCicle = () => {
         enemyPosition++;
         enemyObj.style.right = `${enemyPosition}px`;
         document.getElementById("console").innerText = `Player Position: ${playerPosition[0]}, ${playerPosition[1]}\nEnemy Position: ${enemyPosition}\nGame Points: ${gamePoints}\nGame Situation: ${gameSituation}`
-
         checkForColission();
         if (enemyPosition >= 900) {
             clearInterval(enemyInterval);
@@ -77,31 +71,20 @@ const enemyLifeCicle = () => {
                 clearInterval(fallInterval);
                 gameSituation = "Win";
                 document.getElementById("console").innerText = `Player Position: ${playerPosition[0]}, ${playerPosition[1]}\nEnemy Position: ${enemyPosition}\nGame Points: ${gamePoints}\nGame Situation: ${gameSituation}`
-
-
             } else enemyLifeCicle();
-
         }
-
     }, 1);
-
-
-
 }
 document.addEventListener("DOMContentLoaded", enemyLifeCicle)
 
 const checkForColission = () => {
-
     let playerY = playerPosition[1];
     let enemyX = parseInt(enemyObj.style.right);
-    let enemyY = 50;
-
     if (enemyX >= 754 && enemyX <= 824 && playerY >= 350) {
         clearInterval(enemyInterval);
         clearInterval(jumpInterval);
         clearInterval(fallInterval);
         gameSituation = "loose";
         document.getElementById("console").innerText = `Player Position: ${playerPosition[0]}, ${playerPosition[1]}\nEnemy Position: ${enemyPosition}\nGame Points: ${gamePoints}\nGame Situation: ${gameSituation}`
-
     }
 }
