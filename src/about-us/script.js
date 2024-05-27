6d0a8ff7f875d8cff830697889d15e8450dd814d
PirateCoder13 <boissanetto888@gmail.com>
2024-05-27 10:36:09 -0300

    new rooms and walls

---
+++ src/about-us/script.js
@@ -0,0 +1,16 @@
+
+document.getElementById("back").addEventListener("click", () => {
+    window.electronAPI.setConfig("audio", document.getElementById("range").value);
+
+    window.location.href = "../index.html";
+})
+
+
+document.addEventListener("DOMContentLoaded", fetch("../../config.json")
+    .then((res) => res.text())
+    .then((text) => {
+        let config = JSON.parse(text);
+        document.getElementById("range").value = config.audio;
+    })
+    .catch((e) => console.error(e)));
+
